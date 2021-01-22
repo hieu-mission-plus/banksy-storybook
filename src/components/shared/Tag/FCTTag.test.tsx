9a7b39cbf4fbe6641f4740ef5b05d91fb155afd7
Hoang Doan Manh <hoang.doan@mission.plus>
2021-01-22 16:55:19 +0700

    FB-23 Tag

--- src/components/shared/Tag/FCTTag.test.tsx
+++ src/components/shared/Tag/FCTTag.test.tsx
@@ -3,11 +3,10 @@ import { render } from '@testing-library/react'
 import { ThemeProvider } from '@material-ui/core'
 import theme from '../../../theme'
 import FCTTag from './FCTTag'
-import { action } from '@storybook/addon-actions'
 
 const renderComponent = (view: any) => render(<ThemeProvider theme={theme}>{view}</ThemeProvider>)
 
 it('Should show Tag correctly', () => {
-  const Tag = renderComponent(<FCTTag onDelete={action('onDelete')} />)
+  const Tag = renderComponent(<FCTTag />)
   expect(Tag).toBeTruthy()
 })
